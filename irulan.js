e848f26604704d971f4543cdbf7f95cfef3be61b
Shaun Maher <shaun@ghanima.net>
2015-12-23 19:56:17 +1000

    Local caching all wired up in the server

--- irulan.js
+++ irulan.js
@@ -3,6 +3,9 @@ var app = express();
 var router = express.Router();
 var viewsDir = __dirname + '/views/';
 var clientJsDir = __dirname + '/client_js/';
+var clientCssDir = __dirname + '/client_css/';
+var cachedJsDir = clientJsDir + '/cached/';
+var cachedCssDir = clientCssDir + '/cached/';
 
 router.use(function (req, res, next) {
   console.log("/" + req.method);
@@ -19,6 +22,27 @@ router.get("/components.navbar.js", function (req, res) {
 router.get("/app.js", function (req, res) {
   res.sendFile(clientJsDir + "app.js");
 });
+// Locally cached resources
+router.get("/cached/bootstrap.min.css", function (req, res) {
+  res.sendFile(cachedCssDir + "bootstrap.min.css");
+});
+router.get("/cached/bootstrap.min.js", function (req, res) {
+  res.sendFile(cachedJsDir + "bootstrap.min.js");
+});
+router.get("/cached/jquery.min.js", function (req, res) {
+  res.sendFile(cachedJsDir + "bootstrap.min.js");
+});
+router.get("/cached/angular.min.js", function (req, res) {
+  res.sendFile(cachedJsDir + "angular.min.js");
+});
+router.get("/cached/angular-sanitize.min.js", function (req, res) {
+  res.sendFile(cachedJsDir + "angular-sanitize.min.js");
+});
+router.get("/cached/angular-route.min.js", function (req, res) {
+  res.sendFile(cachedJsDir + "angular-route.min.js");
+});
+
+
 
 app.use("/", router);
 
